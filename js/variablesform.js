bcf476453c6cdf6dd53413b838f414bc12e1a5a8
Franco Gasco <francogasco02@gmail.com>
2022-06-29 20:20:48 -0300

    Eventos y json

--- js/variablesform.js
+++ js/variablesform.js
@@ -5,3 +5,13 @@ const inputMensaje = document.querySelector("#inputMensaje")
 const btnSubmit = document.querySelector("#submit")
 
 let datosDeInput = ""
+
+function guardarDatosDeUsr() {
+    debugger
+    const datosDeUsr = {nombre: inputNombre.value,
+                        telefono: inputTelefono.value,
+                        email: inputEmail.value
+    } 
+    let str = JSON.stringify(datosDeUsr)
+    localStorage.setItem("datosDeUsr", str)
+}
