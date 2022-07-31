1754829978b7a35bb6b77df6d7bafe5d4f5188a7
FrancoGasco <francogasco02@gmail.com>
2022-07-31 17:38:32 -0300

    JS

--- js/asistencia.js
+++ js/asistencia.js
@@ -23,3 +23,79 @@ var dias = document.getElementById("dias")
 btnSubmit.addEventListener("mousemove", ()=> {
     btnSubmit.title = "Complete los datos para que pueda contratar una asistencia"
 })
+
+
+//Base de datos
+const tipoAsistencia = [{"tipo": "Robo", "factor": 1.05},
+                    {"tipo": "Todo riesgo", "factor": 1.15},
+                    {"tipo": "Lesiones medicas", "factor":1.10}]
+
+
+const cantidadDias = 25.6
+
+
+//Variables
+const selectTipoAsistencia = document.querySelector("#selectTipoAsistencia")
+const dias = document.querySelector ("#dias")
+const btnDisponibilidad  = document.querySelector ("#btnDisponibilidad")
+const valorAuto = document.querySelector("#valorAsistencia")
+
+//Cotizacion
+
+class CotizadorAsistencia{
+    constructor (jsonTA, costoDia) {
+        this.arraytipoAsistencia = jsonTA
+        this.dias = costoDia
+    }
+    valorDeAuto(factorAuto, dias){
+        let valorPolizaFinal = (parseFloat(this.dias) * parseFloat(dias)   * parseFloat(factorAsistencia)).toFixed(2)
+        return valorPolizaFinal || "Error"
+    }
+}
+
+//Logica
+
+const cotizador = new CotizadorAsistencia(tipoAsistencia, cantidadDias)
+
+const cargoTipoDeAsistencia = ()=> {
+    let optionTA
+        tipoAsistencia.forEach(element=> {
+            optionTA += `<option value="${element.factor}">${element.tipo}</option>`
+        });
+        return optionTA
+}
+
+const faltanCargarDatos = ()=> {
+    return (isNaN(parseInt(dias)) ||   selectTipoAsistencia.value.trim() == "" )
+}
+
+const muestroCotizacion = ()=> {
+    let tipoAsis = selectTipoAsistencia.value
+    let dia = dias.value
+    let valorDeAsistencia = cotizador.valorDeAsistencia(tipoAsis, dia)
+        valorAsistencia.innerText = `$ ${valorDeAsistencia}`
+}
+
+const cotizarDatosAsistencia = ()=> {
+    faltanCargarDatos() ? toastSA("Complete todos los datos solicitados.", 'darkred') : muestroCotizacion()
+}
+
+const toastSA = (mensaje, bgColor, tiempo)=> {
+    Swal.fire({
+        icon: 'error',
+        title: mensaje,
+        position: 'top-end',
+        showConfirmButton: false,
+        toast: true,
+        timer: tiempo || 3000,
+        timerProgressBar: true,
+        background: bgColor || 'white',
+        color: '#ffffff'
+    })
+}
+
+//app
+selectTipoAsistencia.innerHTML = cargoTipoDeAsistencia()
+
+
+btnDisponibilidad.addEventListener("click", ()=> cotizarDatosAsistencia())
--- js/pasaje.js
+++ js/pasaje.js
@@ -1,30 +1,92 @@
-debugger
-class pasajes {
-    constructor(tipo, cantidad, stock, precio){
-        this.tipo = tipo;
-        this.cantidad = cantidad;
-        this.stock = stock;
-        this.precio = precio;
+//Base de datos
+const tipoButaca = [{"tipo": "Standard", "factor": 1.05},
+                    {"tipo": "VIP", "factor": 1.15},
+                    {"tipo": "Premium", "factor":1.10}]
+
+const lugarDestino = [{"area": "Brasil", "factor": 1.03},
+                    {"area": "Mexico", "factor": 1.07},
+                    {"area": "Estados unidos", "factor":1.10},
+                    {"area": "italia", "factor":1.15 }]
+
+const costoPersona = 29.75
+const cantidadDias = 25.6
+
+
+//Variables
+const selectlugarDestino = document.querySelector("#selectlugarDestino")
+const selectTipoButaca = document.querySelector("#selectTipoButaca")
+const numero = document.querySelector ("#cantidad")
+const dias = document.querySelector ("#dias")
+const btnDisponibilidad  = document.querySelector ("#btnDisponibilidad")
+const valorViaje = document.querySelector("#valorViaje")
+
+//Cotizacion
+
+class CotizadorBoletosAereos{
+    constructor (jsonTB, jsonLD, costoPersona) {
+        this.arraytipoButaca = jsonTB
+        this.arratlugarDestino = jsonLD
+        this.cantidad = costoPersona
     }
-    precioTotal() {
-        let precioFinal = this.precio * this.cantidad;
-        return precioFinal.toFixed(2);
-    }
-    descontarStock(cantidad) {
-        this.stock -=cantidad
+    valorDePasaje(personas, factorButaca, factorDestino, dias){
+        let valorPolizaFinal = (parseFloat(this.cantidad) * parseInt(personas) * parseInt(dias) * parseFloat(factorButaca) * parseFloat (factorDestino)).toFixed(2)
+        return valorPolizaFinal || "Error"
     }
 }
 
-const generarArray =()=>{
-    pasaje.push (new pasajes("Standard", cantidad, 250, 8000));
-    pasaje.push (new pasajes("VIP", cantidad, 25, 13000));
-    pasaje.push (new pasajes("Premium", cantidad, 100, 10000));
+//Logica
+
+const cotizador = new CotizadorBoletosAereos(tipoButaca, lugarDestino, costoPersona)
+
+const cargoLugarDeDestino = ()=>{
+    let optionLD
+        lugarDestino.forEach(element =>{
+            optionLD += `<option value="${element.factor}">${element.area}</option>`
+        });
+        return optionLD
 }
 
-var tipo = document.getElementById("tipo")
-var cantidad = document.getElementById("cantidad")
+const cargoTipoDeButaca = ()=> {
+    let optionTB
+        tipoButaca.forEach(element=> {
+            optionTB+= `<option value="${element.factor}">${element.tipo}</option>`
+        });
+        return optionTB
+}
 
+const faltanCargarDatos = ()=> {
+    return (isNaN(parseInt(dias)) || isNaN(parseInt(cantidad.value)) || selectTipoButaca.value.trim() == "" || selectlugarDestino.value.trim() == "" )
+}
 
-btnSubmit.addEventListener("mousemove", ()=> {
-    btnSubmit.title = "Complete los datos para que se pueda procesar su reserva"
-})
+const muestroCotizacion = ()=> {
+    let pasajeros = cantidad.value
+    let tipoBut = selectTipoButaca.value
+    let lugarDest = selectlugarDestino.value
+    let dia = dias.value
+    let valorDeViaje = cotizador.valorDePasaje(pasajeros, tipoBut, lugarDest, dia)
+        valorViaje.innerText = `$ ${valorDeViaje}`
+}
+
+const cotizarDatosAereo = ()=> {
+    faltanCargarDatos() ? toastSA("Complete todos los datos solicitados.", 'darkred') : muestroCotizacion()
+}
+
+const toastSA = (mensaje, bgColor, tiempo)=> {
+    Swal.fire({
+        icon: 'error',
+        title: mensaje,
+        position: 'top-end',
+        showConfirmButton: false,
+        toast: true,
+        timer: tiempo || 3000,
+        timerProgressBar: true,
+        background: bgColor || 'white',
+        color: '#ffffff'
+    })
+}
+
+//app
+selectTipoButaca.innerHTML = cargoTipoDeButaca()
+selectlugarDestino.innerHTML = cargoLugarDeDestino()
+
+btnDisponibilidad.addEventListener("click", ()=> cotizarDatosAereo())
